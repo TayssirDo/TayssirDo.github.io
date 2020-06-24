35f8842930795744c46274616552ef4f7d887932
TayssirDo <tayssir.doghri@gmail.com>
2020-06-23 22:29:03 -0400

    Update layout

---
+++ _layouts/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="../assets/css/styles.css">
+</head>
+
+<body>
+	<?php include('../includes/header.php');?>
+ 
+	ssssklklksfd
+	<?php include('../includes/footer.php');?>
+	
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" href="assets/css/styles.css">
+	<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css' rel='stylesheet'/>
+</head>
+
+<body>
+	<?php include('includes/header.php');?>
+ 
+	
+	<?php include('includes/footer.php');?>
+	
+</body>
+</html>
